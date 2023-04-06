b8ad90b9f2f7f41ee7ca62c8468825b2d7290438
Stefan <eiftw15688@gmail.com>
2023-04-07 00:30:39 +0200

    Proper final app version

--- receipt_app.py
+++ receipt_app.py
@@ -2,38 +2,33 @@ from requests import get
 
 response = get('https://interview-task-api.mca.dev/qr-scanner-codes/alpha-qr-gFpwhsQ8fkY1')
 produce_list = response.json()
-domestic = []
-imported = []
+produce_list.sort(key=lambda k: k["name"])
 dom_cost = 0
 imp_cost = 0
-
-for item in produce_list:
-    item["description"] = item["description"][:10] + "..."
-    if "weight" not in item:
-        item["weight"] = "N/A"
-    if item["domestic"]:
-        domestic.append(item)
-        dom_cost += item["price"]
-    else:
-        imported.append(item)
-        imp_cost += item["price"]
-
-domestic.sort(key=lambda k: k["name"])
-imported.sort(key=lambda k: k["name"])
+dom_count = 0
+imp_count = 0
 
 print(". Domestic")
-for item in domestic:
-    print(f"... {item['name']}\n    Price: ${item['price']}\n    {item['description']}")
-    if item['weight'] == "N/A":
-        print(f"    Weight: {item['weight']}")
-    else:
-        print(f"    Weight: {item['weight']}g")
+for item in produce_list:
+    if item["domestic"]:
+        dom_count += 1
+        dom_cost += item["price"]
+        print(f"... {item['name']}\n    Price: ${item['price']}\n    {item['description'][:10]}...")
+        if "weight" in item:
+            print(f"    Weight: {item['weight']}g")
+        else:
+            print(f"    Weight: N/A")
+
 print(". Imported")
-for item in imported:
-    print(f"... {item['name']}\n    Price: ${item['price']}\n    {item['description']}")
-    if item['weight'] == "N/A":
-        print(f"    Weight: {item['weight']}")
-    else:
-        print(f"    Weight: {item['weight']}g")
-print(f"Domestic cost: ${dom_cost}\nImported cost: ${imp_cost}\nDomestic count: {len(domestic)}\nImported count: {len(imported)}")
+for item in produce_list:
+    if not item["domestic"]:
+        imp_count += 1
+        imp_cost += item["price"]
+        print(f"... {item['name']}\n    Price: ${item['price']}\n    {item['description'][:10]}...")
+        if "weight" in item:
+            print(f"    Weight: {item['weight']}g")
+        else:
+            print(f"    Weight: N/A")
+
+print(f"Domestic cost: ${dom_cost}\nImported cost: ${imp_cost}\nDomestic count: {dom_count}\nImported count: {imp_count}")
 
